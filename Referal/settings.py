2ece31f6fc9a9f2158a7777f46bda710c79be7bb
Mayank Mishra B <mayank.b.mishra@ericsson.com>
2022-02-11 10:59:31 +0530

    Removed Secret Key and requriments.txt

--- Referal/settings.py
+++ Referal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lo6910sg3ve0=l4(&&s+6z%+-w0gt%(#_s&yrls2%h8_o7u((h'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
